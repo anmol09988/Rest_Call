4770c5a7d2f839b14eb6e92618da13829925b33c
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-07-28 18:07:33 +0530

    updated

--- routes/activity.js
+++ routes/activity.js
@@ -260,11 +260,11 @@ exports.validate = function (req, res) {
         // const accesstokken = obj.access_token;
 
        // console.log("body.access_token"+body);
-        console.log("body2.access_token"+body);
+        console.log("body2.access_token"+body.body);
        // console.log("response.access_token"+accesstokken);
         //console.log("checktokenchecktoken.access_token"+checktoken);
         console.log("response.statusCode"+response);
-        console.log("sssssssssssssssssssssssssssssssssss"+body.get('access_token'));
+      //  console.log("sssssssssssssssssssssssssssssssssss"+body.get('access_token'));
         console.log("respresprespresp"+response.access_token);
     });
 
