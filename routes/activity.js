8cfdad1d4b335a5fd7c1c52a7eea085198a79746
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-07-28 14:52:41 +0530

    updated

--- routes/activity.js
+++ routes/activity.js
@@ -155,29 +155,29 @@ exports.execute = function (req, res) {
 
     // Create JSON body for Fire event
 
-    const https = require('https');
+    // const https = require('https');
 
-    https.get('https://mch4s3mv5j6r7tyf5xqf8s0-y2wm.auth.marketingcloudapis.com/v2/token', res => {
-        let data = [];
-        const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-        console.log('Status Code:', res.statusCode);
-        console.log('Date in Response header:', headerDate);
+    // https.get('https://mch4s3mv5j6r7tyf5xqf8s0-y2wm.auth.marketingcloudapis.com/v2/token', res => {
+    //     let data = [];
+    //     const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+    //     console.log('Status Code:', res.statusCode);
+    //     console.log('Date in Response header:', headerDate);
 
-        res.on('data', chunk => {
-            data.push(chunk);
-        });
+    //     res.on('data', chunk => {
+    //         data.push(chunk);
+    //     });
 
-        res.on('end', () => {
-            console.log('Response ended: ');
-            const users = JSON.parse(Buffer.concat(data).toString());
+    //     res.on('end', () => {
+    //         console.log('Response ended: ');
+    //         const users = JSON.parse(Buffer.concat(data).toString());
 
-            for (user of users) {
-                console.log(`Got user with id: ${user.id}, name: ${user.name}`);
-            }
-        });
-    }).on('error', err => {
-        console.log('Error: ', err.message);
-    });
+    //         for (user of users) {
+    //             console.log(`Got user with id: ${user.id}, name: ${user.name}`);
+    //         }
+    //     });
+    // }).on('error', err => {
+    //     console.log('Error: ', err.message);
+    // });
 
     res.send(200, 'Execute');
     //     } else {
