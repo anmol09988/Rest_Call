02ec0935d565e87d29ece1b056408570f300610c
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-07-29 15:41:45 +0530

    save

--- routes/activity.js
+++ routes/activity.js
@@ -270,7 +270,7 @@ function check(req, res) {
             'Authorization': 'Bearer xxxxxxxxx',
             'Content-Type': 'application/json'
         },
-        url: "https:///mch4s3mv5j6r7tyf5xqf8s0-y2wm.rest.marketingcloudapis.com/messaging/v1/email/messages",
+        url: "hhttps://jsonplaceholder.typicode.com/users?_limit=2",
         method: "POST",
         json: true,   // <--Very important!!!
         body: myJSONObject4
