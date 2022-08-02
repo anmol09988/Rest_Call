f246e9e5484bfe25e0ddad8106e3818149472c37
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-08-02 16:55:38 +0530

    updated

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 'use strict';
 // Module Dependencies
 // -------------------
-var express     = require('express');
-var bodyParser  = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 var errorhandler = require('errorhandler');
-var http        = require('http');
-var path        = require('path');
-var request     = require('request');
-var routes      = require('./routes');
-var activity    = require('./routes/activity');
+var http = require('http');
+var path = require('path');
+var request = require('request');
+var routes = require('./routes');
+var activity = require('./routes/activity');
 
 var app = express();
 
 // Configure Express
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.json({type: 'application/json'})); 
+app.use(bodyParser.json({ type: 'application/json' }));
 //app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(express.methodOverride());
@@ -27,19 +27,41 @@ if ('development' == app.get('env')) {
   app.use(errorhandler());
 }
 
-console.log("checkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+var request = require('request');
+var myJSONObject2 = {
+  "grant_type": "client_credentials",
+  "client_id": "ewozgxquu4nriupcx2tylyfl",
+  "client_secret": "d3BNHjIK6RAZQi7VgbXVYnWw",
+  "account_id": "526000739"
+};
+request({
+  url: "https://mch4s3mv5j6r7tyf5xqf8s0-y2wm.auth.marketingcloudapis.com/v2/token",
+  method: "POST",
+  json: true,   // <--Very important!!!
+  body: myJSONObject2
+}, function (error, response, body) {
+
+  statusCode = response.statusCode;
+  var authorizationtoken = JSON.parse(JSON.stringify(response.body))['access_token'];
+
+  console.log("authorizationtoken"+authorizationtoken);
+
+  if (statusCode === 200) {
+    console.log("INIFLOOP");
+  }
+});
 
 // HubExchange Routes
-app.get('/', routes.index );
-app.post('/login', routes.login );
-app.post('/logout', routes.logout );
+app.get('/', routes.index);
+app.post('/login', routes.login);
+app.post('/logout', routes.logout);
 
 // Custom Hello World Activity Routes
-app.post('/check/save/', activity.save );
-app.post('/check/validate/', activity.validate );
-app.post('/check/publish/', activity.publish );
-app.post('/check/execute/', activity.execute );
+app.post('/check/save/', activity.save);
+app.post('/check/validate/', activity.validate);
+app.post('/check/publish/', activity.publish);
+app.post('/check/execute/', activity.execute);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
