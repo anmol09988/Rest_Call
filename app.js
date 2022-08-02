e4cd38b01e2b268f5528b4e09ce2e9ace62f991a
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-08-02 17:24:32 +0530

    updated

--- app.js
+++ app.js
@@ -27,32 +27,45 @@ if ('development' == app.get('env')) {
   app.use(errorhandler());
 }
 
+var check = "amnmol";
+
+if (check === amnmol) {
+
+  check2();
+}
+
+
 console.log('ccccccccccccccccccchhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh');
-var request = require('request');
-var myJSONObject2 = {
-  "grant_type": "client_credentials",
-  "client_id": "ewozgxquu4nriupcx2tylyfl",
-  "client_secret": "d3BNHjIK6RAZQi7VgbXVYnWw",
-  "account_id": "526000739"
-};
-request({
-  url: "https://mch4s3mv5j6r7tyf5xqf8s0-y2wm.auth.marketingcloudapis.com/v2/token",
-  method: "POST",
-  json: true,   // <--Very important!!!
-  body: myJSONObject2
-}, function (error, response, body) {
 
-  var statusCode = response.statusCode;
-  var authorizationtoken = JSON.parse(JSON.stringify(response.body))['access_token'];
+function check2() {
+  var request = require('request');
+  var myJSONObject2 = {
+    "grant_type": "client_credentials",
+    "client_id": "ewozgxquu4nriupcx2tylyfl",
+    "client_secret": "d3BNHjIK6RAZQi7VgbXVYnWw",
+    "account_id": "526000739"
+  };
+  request({
+    url: "https://mch4s3mv5j6r7tyf5xqf8s0-y2wm.auth.marketingcloudapis.com/v2/token",
+    method: "POST",
+    json: true,   // <--Very important!!!
+    body: myJSONObject2
+  }, function (error, response, body) {
 
-  console.log("authorizationtoken"+authorizationtoken);
+    var statusCode = response.statusCode;
+    var authorizationtoken = JSON.parse(JSON.stringify(response.body))['access_token'];
 
-  if (statusCode === 200) {
-    console.log("INIFLOOP");
-  }
-});
+    console.log("authorizationtoken" + authorizationtoken);
 
-console.log('sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss');
+    if (statusCode === 200) {
+      console.log("INIFLOOP");
+    }
+  });
+
+  console.log('sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss');
+
+}
+
 
 // HubExchange Routes
 app.get('/', routes.index);
